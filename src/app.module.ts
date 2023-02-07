f1d05cc2ab2953ac175edcf79bc6ea5f38250905
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-02-07 17:43:35 +0600

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { CacheModule, Module, CacheInterceptor,process } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  imports: [
+    PokemonModule,
+    // Basic throttling/rate-limiting setup.
+    ThrottlerModule.forRootAsync({
+      useFactory: () => ({
+        ttl: Number(process.env.THROTTLE_TTL),
+        limit: Number(process.env.THROTTLE_LIMIT),
+      }),
+    }),
+    // Basic inmemory caching based on the request-urls.
+    CacheModule.registerAsync({
+      useFactory: () => ({
+        ttl: Number(process.env.CACHE_TTL),
+        max: Number(process.env.CACHE_MAX_ITEMS),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
+})
+export class AppModule {}
